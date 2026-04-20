4e6f2ed1670f8338760a841edfe85dcb007962e1
harjot-jaswal-dev <harjotjaswal.dev@gmail.com>
2026-04-19 22:25:55 -0700

    collision logic implemented

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import pygame
 from constants import *
-from logger import log_state
-from player import *
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable, ) # need to comma to show its a tuple
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_obj = AsteroidField()
     while True:
         dt = clock.tick(60) / 1000
         log_state()
@@ -16,12 +26,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player1.update(dt)
-        player1.draw(screen)
+        updatable.update(dt)
+        for obj in asteroids:
+            if obj.collides_with(player1):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.__version__ }")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
